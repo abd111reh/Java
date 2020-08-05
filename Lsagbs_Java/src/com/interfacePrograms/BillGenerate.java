de7bcf4541956ff210cf6d787e25eb1bc6233a1c
gitabdul <babdul9617@gmail.com>
2020-08-05 07:29:07 +0530

    Adding "List"

--- Lsagbs_Java/src/com/interfacePrograms/BillGenerate.java
+++ Lsagbs_Java/src/com/interfacePrograms/BillGenerate.java
@@ -1,10 +1,15 @@
 package com.interfacePrograms;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 import java.util.Scanner;
 
 public class BillGenerate {
 	public static void main(String[] args) {
-		
+		List<Object> list=new ArrayList<Object>();
 		Scanner scanner=new Scanner(System.in);
 		Invoice invoice=new Invoice();
 		System.out.println("Welcome to Our Shopping Center");
@@ -20,12 +25,26 @@ public class BillGenerate {
 		invoice.productPrice(scanner.nextDouble());
 		System.out.println();
 		System.out.println();
-		System.out.println("Customer Name     "+invoice.name);
-		System.out.println("Customer Address  "+invoice.add);
-		System.out.println("Product Name	  "+invoice.product);
-		System.out.println("Product Quantity  "+invoice.qty);
-		System.out.println("Product Price     "+invoice.price);
-		System.out.println("Total Amount      "+invoice.calculateAmount());
+		/*
+		 * System.out.println("Customer Name     "+invoice.name);
+		 * System.out.println("Customer Address  "+invoice.add);
+		 * System.out.println("Product Name	  "+invoice.product);
+		 * System.out.println("Product Quantity  "+invoice.qty);
+		 * System.out.println("Product Price     "+invoice.price);
+		 * System.out.println("Total Amount      "+invoice.calculateAmount());
+		 */
+		list.add("Customer Name     "+invoice.name);
+		list.add("Customer Address  "+invoice.add);
+		list.add("Product Name	  "+invoice.product);
+		list.add("Product Quantity  "+invoice.qty);
+		list.add("Product Price     "+invoice.price);
+		list.add("Total Amount      "+invoice.calculateAmount());
+		for (Object object : list) {
+			System.out.println(object);
+			
+		}
+		
+			
 		
 			
 		}
